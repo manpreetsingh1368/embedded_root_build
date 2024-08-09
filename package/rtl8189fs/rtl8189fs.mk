762f7b63e071f5200366ec07e568e92de71aa187
manpreetsingh1368 <msingh.zkp@gmail.com>
2024-08-08 22:13:33 -0600

    embedded root Build

---
+++ package/rtl8189fs/rtl8189fs.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# rtl8189fs
+#
+################################################################################
+
+RTL8189FS_VERSION = ed6741b52bc998875298117b55d18b0d557c37a6
+RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
+RTL8189FS_LICENSE = GPL-2.0
+
+RTL8189FS_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8189FS=m \
+	KVER=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR)
+
+define RTL8189FS_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_CFG80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MAC80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MMC)
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
